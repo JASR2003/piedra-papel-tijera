107f80821300de8b461770955f75627449d297bc
Jorge Suárez <suarezjorge758@gmail.com>
2023-10-19 14:42:34 -0400

    crear ciclos para opciones de resultados

--- scripts.js
+++ scripts.js
@@ -14,3 +14,27 @@ const result = document.getElementById('result');
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+let playerPoi = 0;
+let machinePoi = 0;
+
+function startGame(numberSelection) {
+    let playerSelection = numberSelection;
+    let machineSelection = getRandomNumber(1,3);
+
+    if (
+        (playerSelection === 1 && machineSelection === 3) ||
+        (playerSelection === 3 && machineSelection === 2) ||
+        (playerSelection === 2 && machineSelection === 1)
+    ) {
+        userWin();
+    } else if (
+        (machineSelection === 1 && playerSelection === 3) ||
+        (machineSelection === 3 && playerSelection === 2) ||
+        (machineSelection === 2 && playerSelection === 1)
+    ) {
+        machineWin();
+    } else {
+        draw();
+    }
+}
